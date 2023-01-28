ff82cfdad8498b53029969cc524b38869f627a2a
Foucsi <julien.foucartreact@gmail.com>
2023-01-28 15:22:01 +0100

    signin signup finsih cote front

--- components/Signin.js
+++ components/Signin.js
@@ -1,15 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const Container = styled.div`
-  height: 100%;
+  height: 80%;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 `;
 
-export default function Signin() {
-  return <Container>Signin</Container>;
+const Input = styled.input`
+  width: 80%;
+  height: 10%;
+`;
+
+export default function Signup() {
+  const router = useRouter();
+  return (
+    <Container>
+      <div
+        style={{
+          height: "80%",
+          width: "80%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Input placeholder="Username" />
+        <Input type="password" placeholder="password" />
+
+        <button onClick={() => router.push("/welcome")}>LOGIN</button>
+      </div>
+    </Container>
+  );
 }
--- components/Signup.js
+++ components/Signup.js
@@ -1,15 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const Container = styled.div`
-  height: 100%;
+  height: 80%;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+`;
+
+const Input = styled.input`
+  width: 80%;
+  height: 10%;
 `;
 
 export default function Signup() {
-  return <Container>Signup</Container>;
+  const router = useRouter();
+  return (
+    <Container>
+      <div
+        style={{
+          height: "80%",
+          width: "80%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Input placeholder="Username" />
+        <Input placeholder="Email" />
+        <Input type="password" placeholder="password" />
+
+        <button onClick={() => router.push("/welcome")}>SIGN UP</button>
+      </div>
+    </Container>
+  );
 }
